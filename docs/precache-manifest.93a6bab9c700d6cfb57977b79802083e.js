51043e38e2d1da0c85edee0e7ad0cbf58c04b26a
TaeSe0ng Kim (taese0_0ng) <taese0ngjp@gmail.com>
2020-01-13 23:45:55 +0900

    git pages add

---
+++ docs/precache-manifest.93a6bab9c700d6cfb57977b79802083e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/portfolio/robots.txt"
+  },
+  {
+    "revision": "401acf47a81eadb86b9a",
+    "url": "/portfolio/js/chunk-vendors.d18cfe6b.js"
+  },
+  {
+    "revision": "6005fe63939070efc8ec",
+    "url": "/portfolio/js/app.f5b47912.js"
+  },
+  {
+    "revision": "87453224d9b6078aff55",
+    "url": "/portfolio/js/about.2e414ae4.js"
+  },
+  {
+    "revision": "57b1231907b0b2d3873032714327f690",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/portfolio/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "6005fe63939070efc8ec",
+    "url": "/portfolio/css/app.dde1df84.css"
+  }
+];
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about taese0ng's portfolio page</h1>
+    <h1>with github pages</h1>
   </div>
 </template>
